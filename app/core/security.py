9af27c119ec8ceb200ff6bac1418b55fafe82565
Nite2005 <kumarnitesh140905@gmail.com>
2025-09-05 18:21:37 +0530

    app/core/config and app/core/security

---
+++ app/core/security.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timezone, timedelta
+from jose import jwt, JWTError
+from app.core.config import setting
+
+
+
+def create_token(data:dict, expire_minutes=30 ):
+    to_encode = data.copy()
+    expire_time = datetime.now(timezone.utc) + timedelta(minutes=expire_minutes)
+    to_encode.update({'exp': expire_time})
+    return jwt.encode(
+        to_encode,setting.JWT_SECRET_KEY,
+        algorithm=setting.JWT_ALGORITHM
+    )
+
+
+def verify_token(token:str):
+    try:
+        payload = jwt.decode(
+            token, setting.JWT_SECRET_KEY, algorithms=(setting.JWT_ALGORITHM)
+        )
+        return payload
+    except JWTError:
+        return None
